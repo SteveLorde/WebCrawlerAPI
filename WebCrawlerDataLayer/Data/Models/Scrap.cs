f13f91d87ff5a1ab61a718dedd57ba21b9bd5319
SteveLorde <mostafa.maher98@gmail.com>
2024-04-01 08:27:52 +0200

    created Storage folder and added Id and Title properties to Scrap.cs model

--- WebCrawlerDataLayer/Data/Models/Scrap.cs
+++ WebCrawlerDataLayer/Data/Models/Scrap.cs
@@ -2,6 +2,8 @@
 
 public class Scrap
 {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
     public List<string> Extracteddata { get; set; }
     public List<string> Extractedurls { get; set; }
 }
